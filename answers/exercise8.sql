5bf100ac7a1de3b9b8d97e8cfbaccc12ea207f0d
thienle77 <tle9677@gmail.com>
2022-11-30 22:00:25 -0500

    halfway done

---
+++ answers/exercise8.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Students (
+    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    studentName VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    postalCode VARCHAR(255),
+    country VARCHAR(255) NOT NULL
+);
---
+++ answers/exercise9.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+  Students (studentName, address, city, postalCode, country)
+VALUES
+  (
+    'Jane Doe',
+    '57 Union St',
+    'Glasgow',
+    'G13RB',
+    'Scotland'
+  );
